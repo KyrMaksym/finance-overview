634496fe688a8ed51f0133b6275b1c5ba3109649
bsyvuliak <bsyvuliak@datas-tech.com>
2017-06-22 17:51:03 +0200

    all properties

--- src/main/java/config/CamelConfig.java
+++ src/main/java/config/CamelConfig.java
@@ -2,6 +2,7 @@ package config;
 
 import org.apache.camel.CamelContext;
 import org.apache.camel.impl.DefaultCamelContext;
+import org.apache.camel.spring.javaconfig.CamelConfiguration;
 import org.apacheextras.camel.jboss.JBossPackageScanClassResolver;
 import org.springframework.context.annotation.*;
 
@@ -9,16 +10,17 @@ import org.springframework.context.annotation.*;
  * Created by Администратор on 17.06.2017.
  */
 @Configuration
+@ComponentScan("route")
 @Import(ActiveMQConfig.class)
-public class CamelConfig {
+public class CamelConfig extends CamelConfiguration{
 
     //@Profile("jboss")
-    @Bean
+    /*@Bean
     public CamelContext camelContext() {
         System.out.println("CamelContext is up!!!!!!");
         final CamelContext context =  new DefaultCamelContext();
         //context.setPackageScanClassResolver(new JBossPackageScanClassResolver());
         return context;
-    }
+    }*/
 
 }
